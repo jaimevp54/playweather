6f5f0f60ff594e8e8841420e6e905cd337d60d16
Jaime Viñas <jaimevp54@gmail.com>
2018-04-04 14:07:46 -0400

    [changed] stations index page

--- playweather/settings.py
+++ playweather/settings.py
@@ -107,8 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
-
-LANGUAGE_CODE = 'es-ES'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
